f37771a687952393708deb371070fd2da60d2587
Shiyas-N <muhammedshiyasn811@gmail.com>
2023-12-26 22:18:46 +0530

    New Campaign

--- src/assets/Components/ActiveCampaign.jsx
+++ src/assets/Components/ActiveCampaign.jsx
@@ -6,6 +6,15 @@ const ActiveCampaign = ({ campaignRef }) => {
       <h1 className="text-3xl font-medium">Active Campaign</h1>
       <div className="flex overflow-x-scroll pb-10 hide-scroll-bar mt-10">
         <div className="flex flex-nowrap ">
+          <Link to="/campaign/U4Aes">
+            <div className="inline-block px-3">
+              <img
+                src="Campaign3.jpg"
+                alt=""
+                className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+              />
+            </div>
+          </Link>
           <Link to="/campaign/QXyOx7">
             <div className="inline-block px-3">
               <img
@@ -24,16 +33,6 @@ const ActiveCampaign = ({ campaignRef }) => {
               />
             </div>
           </Link>
-
-          {/* <div className="inline-block px-3">
-            <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"></div>
-          </div>
-          <div className="inline-block px-3">
-            <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"></div>
-          </div>
-          <div className="inline-block px-3">
-            <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"></div>
-          </div> */}
         </div>
       </div>
     </div>
