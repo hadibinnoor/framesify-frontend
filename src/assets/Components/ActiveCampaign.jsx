67a03c6c9dc694b0398f1fe430c21934cddf4828
Shiyas-N <muhammedshiyasn811@gmail.com>
2024-02-04 07:29:52 +0530

    Content:New Campaign Added

--- src/assets/Components/ActiveCampaign.jsx
+++ src/assets/Components/ActiveCampaign.jsx
@@ -6,6 +6,24 @@ const ActiveCampaign = ({ campaignRef }) => {
       <h1 className="text-3xl font-medium">Running Campaigns</h1>
       <div className="flex overflow-x-scroll pb-10 hide-scroll-bar mt-10">
         <div className="flex flex-nowrap ">
+          <Link to="/campaign/LlX93">
+            <div className="inline-block px-3">
+              <img
+                src="Campaign9.png"
+                alt=""
+                className="object-cover object-bottom w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+              />
+            </div>
+          </Link>
+          <Link to="/campaign/2kmoYz">
+            <div className="inline-block px-3">
+              <img
+                src="Campaign8.jpg"
+                alt=""
+                className="object-cover w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+              />
+            </div>
+          </Link>
           <Link to="/campaign/Kha2Ji">
             <div className="inline-block px-3">
               <img
