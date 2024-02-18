f108cf0b4a7a1fcd2a5ebc0c57191e609358513d
Hadi Bin Noor <43814202+hadibinnoor@users.noreply.github.com>
2024-02-18 12:28:08 +0530

    Update PricingPage.jsx

--- src/assets/PricingPage.jsx
+++ src/assets/PricingPage.jsx
@@ -86,20 +86,7 @@ const PricingPage = () => {
                 >
                   <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                 </svg>
-                <h2 className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
-                  No Ads
-                </h2>
-              </li>
-              <li className="flex">
-                <svg
-                  className="flex-shrink-0 w-4 h-4 text-blue-600 dark:text-blue-500"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                </svg>
+                
                 <h2 className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400 ms-3">
                   Customer Support
                 </h2>
