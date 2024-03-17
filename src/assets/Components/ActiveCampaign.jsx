e0419e726b0962302308ea93f7079aac1153ade9
Shiyas-N <muhammedshiyasn811@gmail.com>
2024-03-17 23:17:41 +0530

    New campaigns added

--- src/assets/Components/ActiveCampaign.jsx
+++ src/assets/Components/ActiveCampaign.jsx
@@ -3,9 +3,27 @@ import { Link } from "react-router-dom";
 const ActiveCampaign = ({ campaignRef }) => {
   return (
     <div className="" ref={campaignRef}>
-      <h1 className="text-3xl font-medium">Running Campaigns</h1>
+      <h1 className="text-3xl font-medium">Our Happy Customers</h1>
       <div className="flex overflow-x-scroll pb-10 hide-scroll-bar mt-10">
         <div className="flex flex-nowrap ">
+          <Link to="/campaign/VSyaO8">
+            <div className="inline-block px-3">
+              <img
+                src="Campaign11.jpg"
+                alt=""
+                className="object-cover object-bottom w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+              />
+            </div>
+          </Link>
+          <Link to="/campaign/ccLs22">
+            <div className="inline-block px-3">
+              <img
+                src="Campaign10.jpg"
+                alt=""
+                className="object-fill object-bottom w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"
+              />
+            </div>
+          </Link>
           <Link to="/campaign/LlX93">
             <div className="inline-block px-3">
               <img
