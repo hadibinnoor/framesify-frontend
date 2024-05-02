5df6a48a87cc9a1639d51fa41ff1b6a5449cb6eb
Shiyas-N <muhammedshiyasn811@gmail.com>
2024-05-02 23:50:20 +0530

    SslcPage:Button  Design

--- src/assets/Components/Banner.jsx
+++ src/assets/Components/Banner.jsx
@@ -1,4 +1,3 @@
-import { XMarkIcon } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
 
 const Banner = () => {
--- src/assets/SslcPage.jsx
+++ src/assets/SslcPage.jsx
@@ -32,16 +32,16 @@ const SslcPage = () => {
               </ol>
             </div>
           </div>
-          <img
-            src="./sslcPoster.jpg"
-            alt="poster"
-            className="w-[250px] mx-auto"
-          />
           <Link to="/campaign/sw7LB4 " className="mx-auto">
             <button className="w-fit  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
               Click Here
             </button>
           </Link>
+          <img
+            src="./sslcPoster.jpg"
+            alt="poster"
+            className="w-[250px] mx-auto"
+          />
         </div>
       </div>
       <Footer />
