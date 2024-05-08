87979c5783a922e585df4d82f0425a6d6bd557fd
Shiyas-N <muhammedshiyasn811@gmail.com>
2024-05-08 17:33:04 +0530

    Button style

--- src/assets/SslcPage.jsx
+++ src/assets/SslcPage.jsx
@@ -44,7 +44,7 @@ const SslcPage = () => {
           />
           <a
             href="https://wa.me/message/MCLGP4BB4PSHC1"
-            className="w-fit mx-auto shadow-xl text-white bg-[#25D366] hover:bg-[#25D366]/90 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5  focus:outline-none dark:focus:ring-blue-800"
+            className="w-fit mx-auto shadow-xl text-white bg-[#25D366] hover:bg-[#25D366]/90 focus:ring-1 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5  focus:outline-none"
           >
             Contact us for school wise posters
           </a>
